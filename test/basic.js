c8075804942ca3ddd8bf269d1d34e5ac6b81985b
Sergey Korotkov <sergey@pushok.com>
2013-03-29 16:25:52 +0400

    Add sorting and mockup all Cursor methods

--- test/basic.js
+++ test/basic.js
@@ -43,6 +43,7 @@ var mongo = false;
 var num = 1000;
 var paths = {};
 var gt0sin = 0;
+var _dt = null;
 
 function getDb(tag,drop,cb) {
 	if (mongo) {
@@ -95,7 +96,10 @@ vows.describe('Basic').addBatch({
 					var i=0;
 					async.whilst(function () { return i<num}, 
 						function (cb) {
-							var obj = {num:i,sin:Math.sin(i),cos:Math.cos(i),t:15,junk:loremIpsum({count:1,units:"paragraphs"})};
+							var d = new Date();
+							if (_dt==null)
+								_dt=d;
+							var obj = {_dt:d, num:i, pum:i, sin:Math.sin(i),cos:Math.cos(i),t:15,junk:loremIpsum({count:1,units:"paragraphs"})};
 							coll.insert(obj, cb);
 							if (obj.sin>0 && obj.sin<0.5)
 							   gt0sin++;
@@ -105,7 +109,6 @@ vows.describe('Basic').addBatch({
 					)
 				},
 				"ok":function() {
-					console.log(gt0sin);
 					},
 				"has proper size":{
 					topic:function (coll) {
@@ -133,7 +136,9 @@ vows.describe('Basic').addBatch({
 				db.collection("test", {}, safe.sure(cb,function (coll) {
 					coll.ensureIndex({sin:1}, safe.sure(cb, function () {
 						coll.ensureIndex({num:1}, safe.sure(cb, function () {
-							cb(null, coll);
+							coll.ensureIndex({_dt:1}, safe.sure(cb, function () {							
+								cb(null, coll);
+							}))
 						}))
 					}))
 				}))
@@ -158,11 +163,26 @@ vows.describe('Basic').addBatch({
 							else docs.toArray(cb)
 					})
 				},
-				"ok":function (err, docs) {
+				"got it":function (err, docs) {
 					assert.equal(docs[0].num, 10);
 					assert.equal(docs.length, 1);
+				},
+				"date field holds date":function (err, docs) {
+					assert.equal(_.isDate(docs[0]._dt),true);
 				}
 			},
+			"dummy find date":{
+				topic:function (coll) {
+					var cb = this.callback;
+					coll.find({"_dt":_dt}, function (err,docs) {
+						if (err) cb(err);
+							else docs.toArray(cb)
+					})
+				},
+				"got it":function (err, docs) {
+					assert.equal(docs[0]._dt.toString(), _dt.toString());
+				}
+			},			
 			"dummy find $gt":{
 				topic:function (coll) {
 					var cb = this.callback;
@@ -174,7 +194,51 @@ vows.describe('Basic').addBatch({
 				"ok":function (err, docs) {
 					assert.equal(docs.length, gt0sin);
 				}
-			}
+			},
+			"dummy sort {num:1}":{
+				topic:function (coll) {
+					var cb = this.callback;
+					coll.find({num:{$lt:11}}).sort({num:1}).toArray(cb);
+				},
+				"ok":function (err, docs) {
+					assert.equal(err,null);					
+					assert.equal(docs.length, 11);
+					assert.equal(docs[0].num,0);
+				}
+			},
+			"dummy sort {num:-1}":{
+				topic:function (coll) {
+					var cb = this.callback;
+					coll.find({num:{$lt:11}}).sort({num:-1}).toArray(cb);
+				},
+				"ok":function (err, docs) {
+					assert.equal(err,null);
+					assert.equal(docs.length, 11);
+					assert.equal(docs[0].num,10);
+				}
+			},		
+			"dummy sort {pum:1}":{
+				topic:function (coll) {
+					var cb = this.callback;
+					coll.find({pum:{$lt:11}}).sort({pum:1}).toArray(cb);
+				},
+				"ok":function (err, docs) {
+					assert.equal(err,null);					
+					assert.equal(docs.length, 11);
+					assert.equal(docs[0].pum,0);
+				}
+			},
+			"dummy sort {pum:-1}":{
+				topic:function (coll) {
+					var cb = this.callback;
+					coll.find({pum:{$lt:11}}).sort({pum:-1}).toArray(cb);
+				},
+				"ok":function (err, docs) {
+					assert.equal(err,null);
+					assert.equal(docs.length, 11);
+					assert.equal(docs[0].pum,10);
+				}
+			}			
 		}
 	}
 }).export(module);
