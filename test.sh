46962deb762acf5b801e34dc18e665bed5981a63
Alexander Somov <alexs@local.pushok.com>
2013-04-24 22:27:19 +0400

    Use mocha to run integra tests

--- test.sh
+++ test.sh
@@ -4,5 +4,5 @@ cd ./test
 ../node_modules/.bin/vows import-test.js search-test.js search-array-test.js --isolate
 ../node_modules/.bin/mocha basic-test.js
 cd ./contrib
-node ./test/runner.js -t functional
+../../node_modules/.bin/mocha -t 10000 test/mocher.js
 cd ../..
