b91695e90deb8a5e81440882954bb11754a4d85b
Alexander Somov <alexs@local.pushok.com>
2013-12-17 21:27:10 +0400

    Improved test for compound indexes

--- test/index-test.js
+++ test/index-test.js
@@ -1,119 +1,196 @@
-var assert = require('assert');
 var _ = require('lodash');
+var assert = require('assert');
 var async = require('async');
 var safe = require('safe');
 var tutils = require("./utils");
 
-function eq(docs1, docs2) {
-	assert.equal(docs1.length, docs2.length);
-	_(docs1).each(function (a, i) {
-		var b = docs2[i];
-		assert.equal(a.a, b.a);
-		assert.equal(a.b, b.b);
-		assert.equal(a.s, b.s);
-	});
+var dataset = [
+	{ a: 1, b: 1, s: 'Hello', n: 0 },
+	{ a: 1, b: 2, s: 'World', n: 1 },
+	{ a: 1, b: 3, s: 'this', n: 2 },
+	{ a: 2, b: 1, s: 'is', n: 3 },
+	{ a: 2, b: 2, s: 'test', n: 4 },
+	{ a: 3, b: 1, s: 'index', n: 5 },
+	{ a: 2, b: 1, s: 'cat', n: 6 },
+	{ a: 4, b: -1, s: 'dog', n: 7 }
+];
+
+var indexes = [
+	{
+		name: 'single field',
+		value: { a: 1 }
+	},
+	{
+		name: 'another single field',
+		value: { b: -1 }
+	},
+	{
+		name: '2-field',
+		value: [ [ 'a', 1 ], [ 'b', -1 ] ]
+	},
+	{
+		name: 'another 2-field',
+		value: [ [ 's', 'desc' ], [ 'a', 'desc' ] ]
+	},
+	{
+		name: '3-field',
+		value: [ [ 'a', -1 ], [ 'b', 1 ], [ 's', -1 ] ]
+	},
+];
+
+var queries = [
+	{
+		name: 'empty',
+		value: { }
+	},
+	{
+		name: 'by index single field',
+		value: { a: 1 }
+	},
+	{
+		name: 'by index another single field',
+		value: { b: 1 }
+	},
+	{
+		name: 'no index single field',
+		value: { n: 3 }
+	},
+	{
+		name: 'by index 2-field',
+		value: { a: 2, b: 1 }
+	},
+	{
+		name: 'by index another 2-field',
+		value: { a: 2, s: 'test' }
+	},
+	{
+		name: 'partially by index 2-field',
+		value: { a: 2, n: 4 }
+	},
+	{
+		name: 'partially by index another 2-field',
+		value: { s: 'index', b: 1 }
+	},
+	{
+		name: 'by index 3-field',
+		value: { a: 2, b: 1, s: 'is' }
+	},
+	{
+		name: '4-field',
+		value: { a: 2, b: 1, s: 'cat', n: 6 }
+	},
+	{
+		name: '2-field-no-result',
+		value: { a: 2, b: 7 }
+	},
+];
+
+var sorting = [
+	{
+		name: 'empty',
+		value: [ ]
+	},
+	{
+		name: 'by index',
+		value: [ [ 'a', 1 ] ]
+	},
+	{
+		name: 'by another index',
+		value: [ [ 'b', -1 ] ]
+	},
+	{
+		name: 'by 2-field index',
+		value: [ [ 'a', 1 ], [ 'b', -1 ] ]
+	},
+	{
+		name: 'by 3-field index',
+		value: [ [ 'a', -1 ], [ 'b', 1 ], [ 's', -1 ] ]
+	},
+	{
+		name: 'by index and another field',
+		value: [ [ 'a', 1 ], [ 'n', -1 ] ]
+	},
+	{
+		name: 'by 2-field index and another field',
+		value: [ [ 'a', 1 ], [ 'b', -1 ], [ 'n', -1 ] ]
+	},
+];
+
+function check(query, sort, docs) {
+	// apply query to dataset
+	if (!_.isArray(query)) query = [ query ];
+	var sample = _.uniq(_.flatten(_.map(query, function (keys) {
+		return _.filter(dataset, function (doc) {
+			return _.every(keys, function (v, k) {
+				return doc[k] === v;
+			});
+		});
+	})));
+	// check count
+	assert.equal(sample.length, docs.length);
+	// check order
+	for (var i = 0; i < docs.length - 1; i++) {
+		var doc1 = docs[i];
+		var doc2 = docs[i + 1];
+		for (var j in sort) {
+			var o = sort[j];
+			var k = o[0];
+			var d = o[1];
+			var v1 = doc1[k];
+			var v2 = doc2[k];
+			if (v1 < v2) {
+				assert.equal(d, 1);
+				break;
+			} else if (v1 > v2) {
+				assert.equal(d, -1);
+				break;
+			}
+		}
+	}
 }
 
 describe('Index Test', function () {
-	describe('New store', function () {
-		var db, coll, docs;
-		before(function (done) {
-			tutils.getDb('test', true, safe.sure(done, function (_db) {
-				db = _db;
+	var db, coll;
+	before(function (done) {
+		tutils.getDb('test', true, safe.sure(done, function (_db) {
+			db = _db;
+			done();
+		}));
+	});
+	it("Create new collection", function (done) {
+		db.collection("test", {}, safe.sure(done, function (_coll) {
+			coll = _coll;
+			done();
+		}));
+	});
+	_.each(indexes, function (index) {
+		it("Create " + index.name + " index", function (done) {
+			coll.ensureIndex(index.value, safe.sure(done, function (indexName) {
 				done();
 			}));
 		});
-		it("Create new collection", function (done) {
-			db.collection("test", {}, safe.sure(done, function (_coll) {
-				coll = _coll;
-				done();
-			}));
-		});
-		it("Create single field index", function (done) {
-			coll.ensureIndex({ a: 1 }, safe.sure(done, function (indexName) {
-				assert.equal(indexName, 'a_1');
-				done();
-			}));
-		});
-		it("Create compound index on 2 fields", function (done) {
-			coll.ensureIndex({ a: 1, b: -1 }, safe.sure(done, function (indexName) {
-				assert.equal(indexName, 'a_1_b_-1');
-				done();
-			}));
-		});
-		it("Create compound index on 3 fields", function (done) {
-			coll.ensureIndex({ a: -1, b: 1, s: -1 }, safe.sure(done, function (indexName) {
-				assert.equal(indexName, 'a_-1_b_1_s_-1');
-				done();
-			}));
-		});
-		it("Create another compound index on 2 fields", function (done) {
-			coll.ensureIndex([ [ 'a', 'desc' ], ['s', 'desc' ] ], function (err, indexName) {
-				assert.equal(err, null);
-				assert.equal(indexName, 'a_-1_s_-1');
-				done();
+	});
+	it("Populate with test data", function (done) {
+		async.forEachSeries(dataset, function (doc, cb) {
+			coll.insert(doc, cb);
+		}, done);
+	});
+	_.each(queries, function (query) {
+		_.each(sorting, function (sort) {
+			var name = "Search " + query.name + " query, sort " + sort.name;
+			it(name, function (done) {
+				coll.find(query.value).sort(sort.value).toArray(safe.sure(done, function (docs) {
+					check(query.value, sort.value, docs);
+					done();
+				}));
 			});
-		});
-		it("Populate with test data", function (done) {
-			docs = [
-				{ a: 1, b: 1, s: 'Hello' },
-				{ a: 1, b: 2, s: 'World' },
-				{ a: 1, b: 3, s: 'this' },
-				{ a: 2, b: 1, s: 'is' },
-				{ a: 2, b: 2, s: 'test' },
-				{ a: 3, b: 1, s: 'index' },
-				{ a: 2, b: 1, s: 'cat' },
-				{ a: 4, b: -1, s: 'dog' }
-			];
-			async.forEachSeries(docs, function (doc, cb) {
-				coll.insert(doc, cb);
-			}, done);
-		});
-		it("Sort by index", function (done) {
-			coll.find().sort({ a: 1 }).toArray(safe.sure(done, function (docs) {
-				var exp = docs.slice().sort(function (l, r) { return l.a - r.a; });
-				eq(docs, exp);
-				done();
-			}));
-		});
-		it("Sort by 2-field index", function (done) {
-			coll.find().sort([ [ 'a', 1 ], [ 'b', -1 ] ]).toArray(safe.sure(done, function (docs) {
-				var exp = docs.slice().sort(function (l, r) {
-					var d = l.a - r.a;
-					if (d !== 0) return d;
-					return r.b - l.b;
-				});
-				eq(docs, exp);
-				done();
-			}));
-		});
-		it("Sort by 3-field index", function (done) {
-			coll.find().sort([ [ 'a', -1 ], [ 'b', 1 ], [ 's', -1 ] ]).toArray(safe.sure(done, function (docs) {
-				var exp = docs.slice().sort(function (l, r) {
-					var d = r.a - l.a;
-					if (d !== 0) return d;
-					d = l.b - r.b;
-					if (d !== 0) return d;
-					if (l.s < r.s) return 1;
-					else if (l.s > r.s) return -1;
-					else return 0;
-				});
-				eq(docs, exp);
-				done();
-			}));
-		});
-		it("Search and sort by index", function (done) {
-			coll.find({ a: 1 }).sort([ [ 'a', 1 ], [ 'b', -1 ] ]).toArray(safe.sure(done, function (docs) {
-				var exp = docs.filter(function (v) {
-					return v.a == 1;
-				}).sort(function (l, r) {
-					var d = l.a - r.a;
-					if (d !== 0) return d;
-					return r.b - l.b;
-				});
-				eq(docs, exp);
-				done();
-			}));
+			var rsort = _.map(sort.value, function (v) { return [ v[0], -v[1] ]; });
+			it(name + ", reverse", function (done) {
+				coll.find(query.value).sort(rsort).toArray(safe.sure(done, function (docs) {
+					check(query.value, rsort, docs);
+					done();
+				}));
+			});
 		});
 	});
 });
